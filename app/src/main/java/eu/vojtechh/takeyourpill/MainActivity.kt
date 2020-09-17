81763bd1242bfa1691cb72604eb86ab59c8ce598
Vojtěch Hořánek <vojtechhoranek@gmail.com>
2020-09-17 21:32:33 +0200

    Created app

---
+++ app/src/main/java/eu/vojtechh/takeyourpill/MainActivity.kt
@@ -0,0 +1,11 @@
+package eu.vojtechh.takeyourpill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
