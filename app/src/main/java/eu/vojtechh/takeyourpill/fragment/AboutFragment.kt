d07384f8204710ee0486882389fe7a196b614680
vojta-horanek <vojtechhoranek@gmail.com>
2020-11-11 21:00:50 +0100

    Added animation to SettingsFragment.kt and AboutFragment.kt

--- app/src/main/java/eu/vojtechh/takeyourpill/fragment/AboutFragment.kt
+++ app/src/main/java/eu/vojtechh/takeyourpill/fragment/AboutFragment.kt
@@ -3,14 +3,26 @@ package eu.vojtechh.takeyourpill.fragment
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.material.transition.MaterialSharedAxis
 import eu.vojtechh.takeyourpill.R
 import eu.vojtechh.takeyourpill.databinding.AboutFragmentBinding
+import eu.vojtechh.takeyourpill.klass.Constants
 import eu.vojtechh.takeyourpill.klass.viewBinding
 
 class AboutFragment : Fragment(R.layout.about_fragment) {
 
     private val model: AboutFragment by viewModels()
     private val view by viewBinding(AboutFragmentBinding::bind)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true).apply {
+            duration = Constants.ANIMATION_DURATION
+        }
+        returnTransition = MaterialSharedAxis(MaterialSharedAxis.Z, false).apply {
+            duration = Constants.ANIMATION_DURATION
+        }
+    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
--- app/src/main/java/eu/vojtechh/takeyourpill/fragment/SettingsFragment.kt
+++ app/src/main/java/eu/vojtechh/takeyourpill/fragment/SettingsFragment.kt
@@ -2,9 +2,22 @@ package eu.vojtechh.takeyourpill.fragment
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.material.transition.MaterialSharedAxis
 import eu.vojtechh.takeyourpill.R
+import eu.vojtechh.takeyourpill.klass.Constants
 
 class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true).apply {
+            duration = Constants.ANIMATION_DURATION
+        }
+        returnTransition = MaterialSharedAxis(MaterialSharedAxis.Z, false).apply {
+            duration = Constants.ANIMATION_DURATION
+        }
+
+    }
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
     }
