d23226aaf87d44eee36926da84c8b3b19fd11512
Vojtěch Hořánek <vojtechhoranek@gmail.com>
2020-12-16 20:28:50 +0100

    Basic implementation of AboutActivity.kt

--- app/src/main/java/eu/vojtechh/takeyourpill/activity/AboutActivity.kt
+++ app/src/main/java/eu/vojtechh/takeyourpill/activity/AboutActivity.kt
@@ -1,9 +1,14 @@
 package eu.vojtechh.takeyourpill.activity
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import eu.vojtechh.takeyourpill.BuildConfig
+import eu.vojtechh.takeyourpill.R
 import eu.vojtechh.takeyourpill.databinding.ActivityAboutBinding
 import eu.vojtechh.takeyourpill.klass.viewBinding
+
 
 class AboutActivity : AppCompatActivity() {
     private val binding by viewBinding(ActivityAboutBinding::inflate)
@@ -14,5 +19,13 @@ class AboutActivity : AppCompatActivity() {
         binding.buttonClose.setOnClickListener {
             finish()
         }
+        binding.buttonGithub.setOnClickListener {
+            val browserIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://github.com/vojta-horanek/take-your-pill")
+            )
+            startActivity(browserIntent)
+        }
+        binding.textVersion.text = getString(R.string.version, BuildConfig.VERSION_NAME)
     }
 }
