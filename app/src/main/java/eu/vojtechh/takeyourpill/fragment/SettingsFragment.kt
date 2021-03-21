6db1406b3f60bcb5365e25f04e6c20982f2a860c
Vojtěch Hořánek <vojtechhoranek@gmail.com>
2021-03-21 21:01:44 +0100

    remove useless viewbinding line

--- app/src/main/java/eu/vojtechh/takeyourpill/fragment/SettingsFragment.kt
+++ app/src/main/java/eu/vojtechh/takeyourpill/fragment/SettingsFragment.kt
@@ -3,15 +3,11 @@ package eu.vojtechh.takeyourpill.fragment
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.google.android.material.transition.MaterialFadeThrough
-import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import eu.vojtechh.takeyourpill.R
-import eu.vojtechh.takeyourpill.databinding.FragmentSettingsBinding
 
 @AndroidEntryPoint
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
-
-    private val binding by viewBinding(FragmentSettingsBinding::bind)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
